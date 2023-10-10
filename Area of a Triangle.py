7070fd564592015b1252b3967041439232f59583
lemonntec <miraclechinedum1@gmail.com>
2023-10-10 12:10:06 +0100

    Area of a Triangle

---
+++ Area of a Triangle.py
@@ -0,0 +1,7 @@
+#The sides are in metres
+Base = float(input('Input your base:'))
+Height = float(input('Input your height:'))
+
+Area = float(0.5*Base*Height)
+
+print('The Area of the Traingle is:',Area,'metres')
